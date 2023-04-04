271e578af054cab3da2fe0c2027a988cc65aadc5
Anudeepthi Kolagani <71210233+anudeepthi166@users.noreply.github.com>
2023-04-04 15:02:25 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loginReducer from "../src/slice/loginSlice";
-
-//create store
-export const store = configureStore({
-  reducer: {
-    login: loginReducer,
-  },
-});
